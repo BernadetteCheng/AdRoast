76f8c2ff84cbe3d6f1d7182389a81802c45d36a4
Taha Masood <tahamasood356@gmail.com>
2019-02-23 17:45:40 -0500

    Updated Model

--- Source/Training/TrainModel.py
+++ Source/Training/TrainModel.py
@@ -7,9 +7,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
 
+from sklearn.model_selection import KFold
 from sklearn import ensemble
 from sklearn import linear_model
+from sklearn.model_selection import train_test_split
 import pickle
 
 class Train:
@@ -19,17 +23,26 @@ class Train:
     """
     def __init__(self):
         self.train = {}
-        self.parameters = {'n_estimators' : 10,
-                      'max_depth' : 20,
-                      'learning_rate' : 1,
-                      'criterion' : 'mse'}
+        self.parameters = {'n_estimators' : 1000,
+                           'max_depth' : 2,
+                           'learning_rate' : 0.1,
+                           'criterion' : 'mse'}
 
     """
         Purpose: Train model for adroast application
     """
     def train_model(self, input_features, target_values):
-        gradient_boosting_regressor = ensemble.GradientBoostingRegressor(**self.parameters)
-        gradient_boosting_regressor.fit(input_features, target_values)
+        gradient_boosting_classifier = ensemble.GradientBoostingClassifier(**self.parameters)
+
+        X_train, X_test, y_train, y_test = train_test_split(input_features, target_values, test_size = 0.1, random_state = 22)
+        gradient_boosting_classifier.fit(X_train, y_train)
+
+        y_pred = gradient_boosting_classifier.predict(X_train)
+        print(accuracy_score(y_train, y_pred, normalize=True))
+
+        y_pred = gradient_boosting_classifier.predict(X_test)
+        print(accuracy_score(y_test, y_pred, normalize=True))
 
         filename = 'adroast_model.sav'
-        pickle.dump(gradient_boosting_regressor, open(filename, 'wb'))
+        pickle.dump(gradient_boosting_classifier, open(filename, 'wb'))
+
