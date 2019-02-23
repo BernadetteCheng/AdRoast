9d2b01b2f4aa29e04743675954b5ce21aa43737e
Tony Wang <tony@clinky.io>
2019-02-23 13:32:56 -0500

    Updated Frontend for Webapp

--- Server/AdRoast/AdRoast/urls.py
+++ Server/AdRoast/AdRoast/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from api import views
+from api import routes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
