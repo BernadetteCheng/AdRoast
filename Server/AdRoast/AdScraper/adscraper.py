29ca8acfaa91acc85a3a93a9bbe63fc4a81b2768
Tony Wang <tony@clinky.io>
2019-02-23 06:14:12 -0500

    Updating adscraper

--- Server/AdRoast/AdScraper/adscraper.py
+++ Server/AdRoast/AdScraper/adscraper.py
@@ -5,9 +5,6 @@ class AdScraper(scrapy.Spider):
     start_urls = ['https://transparencyreport.google.com/political-ads/advertiser/AR120847323008860160/creative/CR554897680015294464']
 
     def parse(self, response):
-        SET_SELECTOR = '.set'
-        for imgset in response.css(SET_SELECTOR):
-            IMAGE_SELECTOR = 'div'
-            yield {
-                'img': imgset.css(IMAGE_SELECTOR).extract_first(),
-            }
+        filename = response.url.split("/")[-1] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
