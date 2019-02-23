a28fb349b16f2347a534914231db96d20a41f51f
Tony Wang <tony@clinky.io>
2019-02-23 06:12:32 -0500

    Updating adscraper

--- Server/AdRoast/AdScraper/adscraper.py
+++ Server/AdRoast/AdScraper/adscraper.py
@@ -7,7 +7,7 @@ class AdScraper(scrapy.Spider):
     def parse(self, response):
         SET_SELECTOR = '.set'
         for imgset in response.css(SET_SELECTOR):
-            IMAGE_SELECTOR = 'div a img'
+            IMAGE_SELECTOR = 'div'
             yield {
                 'img': imgset.css(IMAGE_SELECTOR).extract_first(),
             }
