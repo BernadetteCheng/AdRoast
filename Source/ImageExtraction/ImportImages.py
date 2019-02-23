f2e15e682d4c29d858e290c20dc228823df6ecc1
Taha Masood <tahamasood356@gmail.com>
2019-02-23 13:41:41 -0500

    Updated Analytics

--- Source/ImageExtraction/ImportImages.py
+++ Source/ImageExtraction/ImportImages.py
@@ -5,11 +5,13 @@
 """
 
 import cv2
-import matplotlib
-import math
-import pandas as pd
 import numpy as np
-import time
+import scipy
+from scipy import stats
+from scipy.misc import imread
+import random
+import os
+import matplotlib.pyplot as plt
 
 
 class Images:
@@ -80,3 +82,97 @@ class Images:
         img = cv2.drawKeypoints(gray, kp)
 
         cv2.imwrite('sift_keypoints.jpg', img)
+
+    """
+        Purpose: Determines the colorfulness level of a given image
+        Status: Complete
+    """
+    def image_colorfulness(self, image_name):
+        assert self.exists_image(self, image_name), 'Image does not exist!'
+
+        (R,G,B) = cv2.split(self.images[image_name].astype('float'))
+        RG = np.absolute(R - G)
+        YB = np.absolute(((R + G) * 0.5) - B)
+
+        (RGMEAN, RBSTD) = (np.mean(RG), np.std(RG))
+        (YBMEAN, YBSTD) = (np.mean(YB), np.std(YB))
+
+        STANDARD = np.sqrt((RBSTD ** 2) + (YBSTD ** 2))
+        MEAN = np.sqrt((RGMEAN ** 2) + (YBMEAN ** 2))
+
+        COLORFULNESS = (STANDARD + (0.3 * MEAN))
+        return COLORFULNESS
+
+    """
+        Purpose: Provides the number of edges that were observed in the ad
+    """
+    def harris_corner_detection(self, image_name, display_image):
+        assert self.exists_image(self, image_name), 'Image does not exist!'
+
+        gray_component = cv2.cvtColor(self.images[image_name], cv2.COLOR_BGR2GRAY)
+
+        gray_component = np.float32(gray_component)
+        destination = cv2.cornerHarris(gray_component, 2, 3, 0.04)
+        destination = cv2.dilate(destination, None)
+
+        if display_image:
+            self.images[image_name][destination > 0.01 * destination.max()] = [0, 0, 255]
+
+            cv2.imshow('destination', self.images[image_name])
+            if cv2.waitKey(0) & 0xff == 27:
+                cv2.destroyAllWindows()
+
+        return len(destination)
+
+    """
+        Purpose: Analyzes the RGB Histogram of an advertisement
+    """
+    def rgb_hist(self, image_name, display):
+        assert self.exists_image(self, image_name), 'Image does not exist!'
+        colour = ('b', 'g', 'r')
+        rgb_histograms = []
+
+        for i, colour in enumerate(colour):
+            rgb_histogram = cv2.calcHist([self.images[image_name]], [i], None, [256], [0, 256])
+            rgb_histograms.append(rgb_histogram)
+
+            if display:
+                plt.plot(rgb_histogram, color=colour)
+                plt.xlim([0, 256])
+
+        if display:
+            plt.show()
+
+        return(rgb_histograms)
+
+    """
+        Purpose: Analyzes the Histogram of an advertisement
+    """
+    def hist(self, image_name):
+        assert self.exists_image(self, image_name), 'Image does not exist'
+
+        histogram = plt.hist(self.images[image_name].ravel(), 256, [0, 256]);
+        print(type(histogram))
+        plt.show()
+
+    """
+        Purpose: Analyzes specific components of the rgb_histogram
+    """
+    def rgb_hist_analysis(self, image_name):
+        assert self.exists_image(self, image_name), 'Image does not exist'
+
+        specific_amounts = []
+        histograms = self.rgb_hist(image_name, False)
+
+        for histogram in histograms:
+            mean = np.mean(histogram)
+            variance = np.var(histogram)
+            kurtosis = scipy.stats.kurtosis(histogram)
+            skewness = scipy.stats.skew(histogram)
+
+            specific_amounts.append(float(mean))
+            specific_amounts.append(float(variance))
+            specific_amounts.append(float(kurtosis))
+            specific_amounts.append(float(skewness))
+
+        return specific_amounts
--- Source/SimulateTraining.py
+++ Source/SimulateTraining.py
@@ -8,9 +8,11 @@ from ImageExtraction.ImportImages import Images as im
 
 
 def main():
-    clean_data = an()
-    an.import_csv(clean_data, 'C:\\Users\\Taha Masood\\Desktop\\AdRoast\\Source\\Data\\GoogleData\\clean_final.csv', 'final')
-    an.download_images(clean_data, 'final')
+    image_analysis = im()
+    im.import_image(image_analysis, 'Data\\Images\\10795125566885164803.jpg', 'original')
+    im.image_colorfulness(image_analysis, 'original')
+    im.harris_corner_detection(image_analysis, 'original', False)
+    im.rgb_hist_analysis(image_analysis, 'original')
 
 if __name__ == '__main__':
     main()
