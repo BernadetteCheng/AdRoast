ce2a69a7c2c320875e1495583cb89e7109a0ee7c
Tony Wang <tony@clinky.io>
2019-02-24 08:39:07 -0500

    Updating Analysis

--- Server/AdRoast/api/routes.py
+++ Server/AdRoast/api/routes.py
@@ -11,7 +11,7 @@ def analysisPOST(request):
     if request.method == 'POST':
         with open("parsedImage.jpg", "wb") as fh:
             fh.write(base64.decodebytes(request.body))
-        result = extract_feature("P1.jpg")
+        result = extract_feature("P2.jpg")
         data = {
             'grade': str(result[0]),
             'improvements': result[1],
