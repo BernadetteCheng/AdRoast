75c593a0cabce7d985b968661a928f2e5ef8a3a2
Tony Wang <tony@clinky.io>
2019-02-23 22:40:32 -0500

    Updating front end

--- Server/AdRoast/static/assets/js/upload.js
+++ Server/AdRoast/static/assets/js/upload.js
@@ -1,29 +1,27 @@
-(function($) {
-  $img_form = $("#img_upload"),
-  $upload_button = $("#img_upload_button");
+$(document).ready(function() {
+  var $img_form = $("#img_upload"),
+      $upload_button = $("#img_upload_button");
 
   $upload_button.onClick({
     console.log("click");
-    /*
     jQuery.noConflict();
     formdata = new FormData();
     var file = $img_form.prop('files');
     if (formdata) {
         formdata.append("image", file);
-        jQuery.ajax({
-            url: "http://104.248.237.28:1313/analysisPOST",
-            type: "POST",
-            data: formdata,
-            processData: false,
-            contentType: false,
-            success:function(res){
-              console.log('haha');
-            },
-            failure:function(e){
-              console.log('oof');
-            }
-        });
+        $.ajax({
+          type: "POST",
+          url: "http://104.248.237.28:1313/analysisPOST",
+          data: formdata,
+          processData: false,
+          contentType: false,
+          success: function(res){
+            console.log('haha');
+          },
+          error: function(e){
+            console.log('oof');
+          }
+        })
       }
-    */
-    })
-})(jQuery);
+  });
+});
