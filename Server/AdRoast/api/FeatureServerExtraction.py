fd596668aab24c8390403423f2af84e58ddf2ea7
Tony Wang <tony@clinky.io>
2019-02-24 08:20:45 -0500

    Updating Analysis

--- Server/AdRoast/api/FeatureServerExtraction.py
+++ Server/AdRoast/api/FeatureServerExtraction.py
@@ -150,7 +150,7 @@ def top_improvements(feature_list):
     deviances = [edge_deviance, colorfullness_deviance, text_len_deviance, word_len_deviance]
     deviances = deviances.sort(reverse=True)
 
-    print(str(edge_deviance) + str(colorfullness_deviance))
+    print("Test" + str(edge_deviance) + str(colorfullness_deviance))
     updates = [deviances[0], deviances[1]]
     return_information = {}
 
