613643308445192f33ca7bae76701ec0a7f077e9
supermop2525 <omair.sandhu@ryerson.ca>
2019-02-23 20:33:48 -0500

    front end included good sure

--- app/src/main/java/com/example/adroast/response.java
+++ app/src/main/java/com/example/adroast/response.java
@@ -1,17 +1,22 @@
 package com.example.adroast;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.db.chart.animation.Animation;
-import com.db.chart.model.Bar;
-import com.db.chart.model.BarSet;
-import com.db.chart.view.BarChartView;
-import com.db.chart.animation.Animation;
-import com.db.chart.view.ChartView;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.components.AxisBase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class response extends AppCompatActivity {
 
@@ -20,18 +25,33 @@ public class response extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_response);
 
-        BarChartView chart=(BarChartView)findViewById(R.id.barChart);
+        BarChart chart=(BarChart)findViewById(R.id.barChart);
 
-        BarSet dataSet=new BarSet();
-        dataSet.addBar(new Bar("first", 5));  //or whatever data you have
-        dataSet.addBar(new Bar("second",7));
-        dataSet.addBar(new Bar("third", 4));
-        dataSet.addBar(new Bar("forth", 3));
-        chart.addData(dataSet);
-
-        Animation anim = new Animation(6000);
-        anim.fromAlpha(4).fromColor(4);//.setEasing(new LinearEase());     //choose your animation here
-        chart.show(anim);
+        List<BarEntry> entries = new ArrayList<BarEntry>();
+        entries.add(new BarEntry(1,4));
+        entries.add(new BarEntry(2,20));
+        BarDataSet dataSet = new BarDataSet(entries, "Label");
+        dataSet.setColor(1,700);
+        dataSet.setValueTextColor(Color.BLUE);
+        BarData barData = new BarData(dataSet);
+        chart.setData(barData);
+        chart.setDrawGridBackground(false);
+        chart.setGridBackgroundColor(Color.WHITE);
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(10f);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setDrawAxisLine(true);
+        xAxis.setDrawGridLines(false);
+        YAxis left = chart.getAxisLeft();
+        
+        left.setDrawLabels(false);
+        left.setDrawAxisLine(false);
+        left.setDrawGridLines(false);
+        left.setDrawZeroLine(true);
+        chart.getAxisRight().setEnabled(false);
+        left.setTextColor(Color.WHITE);
+        chart.invalidate();
 
         Button cont = (Button) findViewById(R.id.button7);
         cont.setOnClickListener(new View.OnClickListener() {
